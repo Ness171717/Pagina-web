4bcd968e85588122469a43e18e2affc2f16c2462
Ness171717 <paola.delrio4801@alumnos.udg.mx>
2024-12-27 14:20:13 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Seleccionar todos los carruseles
+const carousels = document.querySelectorAll('.image-carousel');
+
+carousels.forEach((carousel) => {
+    const container = carousel.querySelector('.carousel-container');
+    const images = container.querySelectorAll('img');
+    const prevButton = carousel.querySelector('.carousel-prev');
+    const nextButton = carousel.querySelector('.carousel-next');
+
+    let currentIndex = 0; // Índice inicial
+    const totalImages = images.length; // Número total de imágenes en este carrusel
+
+    function updateCarousel() {
+        const offset = -currentIndex * 100; // Desplazamiento dinámico
+        container.style.transform = `translateX(${offset}%)`;
+    }
+
+    nextButton.addEventListener('click', () => {
+        if (currentIndex < totalImages - 1) {
+            currentIndex++;
+            updateCarousel();
+        }
+    });
+
+    prevButton.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateCarousel();
+        }
+    });
+});
