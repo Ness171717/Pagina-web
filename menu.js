9917d7571ce6b8e0b83eeb52d902fa6156574dc2
Ness171717 <paola.delrio4801@alumnos.udg.mx>
2025-01-17 21:26:18 -0600

    clic menu funcionando

--- menu.js
+++ menu.js
@@ -1,17 +1,37 @@
-let currentIndex = 0;
-const items = document.querySelectorAll('.carousel-item');
-const totalItems = items.length;
-
-function moveSlide() {
-  currentIndex++;
-  if (currentIndex >= totalItems) {
-    currentIndex = 0;
-  }
-
-  // Mover el carrusel a la posición correcta
-  const offset = -currentIndex * 100;
-  document.querySelector('.carousel-track').style.transform = `translateX(${offset}%)`;
-}
-
-// Establecer el intervalo para el deslizamiento automático (cada 3 segundos)
-setInterval(moveSlide, 3000); // Cambia las imágenes cada 3 segundos
+document.addEventListener("DOMContentLoaded", function () {
+    // Carrusel automático
+    let currentIndex = 0;
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+  
+    function moveSlide() {
+      currentIndex++;
+      if (currentIndex >= totalItems) {
+        currentIndex = 0;
+      }
+      const offset = -currentIndex * 100;
+      document.querySelector('.carousel-track').style.transform = `translateX(${offset}%)`;
+    }
+    setInterval(moveSlide, 3000);
+   });
+    // Menú toggle
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuToggle = document.querySelector(".menu-toggle");
+        const mobileMenu = document.querySelector(".mobile-menu");
+      
+        // Mostrar u ocultar el menú al hacer clic en el botón
+        menuToggle.addEventListener("click", function (event) {
+          mobileMenu.classList.toggle("active"); // Agregar o quitar la clase 'active'
+          event.stopPropagation(); // Evitar que el clic se propague al documento
+        });
+      
+        // Cerrar el menú si haces clic fuera de él
+        document.addEventListener("click", function (event) {
+          if (!mobileMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+            mobileMenu.classList.remove("active"); // Quitar la clase 'active'
+          }
+        });
+      });
+      
+      
+  
